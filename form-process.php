b461723424ba56948b2ad1ac1507579e99215582
Andrew <ajw35@pdx.edu>
2023-04-02 10:30:24 -0500

    added fontawesome

---
+++ form-process.php
@@ -0,0 +1,8 @@
+<?php
+ $nombres = $_POST["nombres"];
+ $apellidos = $_POST["apellidos"];
+ $correo = $_POST["correo"];
+ $celular = $_POST["celular"];
+ $asunto = $_POST["asunto"];
+ $comentarios = $_POST["comentarios"];
+?>
