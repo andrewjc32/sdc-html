4b7fad9f1f1f5fcde913faf38874c922772a956f
Andrew <ajw35@pdx.edu>
2023-04-03 18:46:26 -0500

    pqrs email sender

--- form-process.php
+++
@@ -1,8 +0,0 @@
-<?php
- $nombres = $_POST["nombres"];
- $apellidos = $_POST["apellidos"];
- $correo = $_POST["correo"];
- $celular = $_POST["celular"];
- $asunto = $_POST["asunto"];
- $comentarios = $_POST["comentarios"];
-?>
